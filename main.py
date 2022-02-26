7a061fd106696d2563cdd671688b4fde2756c5e2
mitsuvlad <mitsuvlad1@gmail.com>
2022-02-26 19:14:50 +0300

    1 tast

---
+++ main.py
@@ -0,0 +1,13 @@
+#1
+import requests
+import json
+url = 'https://api.github.com/users/mitsuvlad/repos'
+response = requests.get(url)
+j_data = response.json()
+with open('repo_data.json', 'w') as f:
+    json.dump(j_data,f)
+for i in j_data:
+    print(i['name'])
+
+#2
+
